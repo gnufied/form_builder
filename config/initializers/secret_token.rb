faf1bc631448ccb237ef28ddfb203cf8935ba5e4
Hemant Kumar <gethemant@gmail.com>
2012-08-09 11:55:53 +0530

    Add crap

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FormBuilder::Application.config.secret_token = 'f6c3abf458d2d4f98078737d95906ec893f80093f9cc2c8deaa0532c445a02c05e88ca14ead1c551f00b213bb1c2fd3ddf568e9ce9d1cb6812e40fce7b673146'
